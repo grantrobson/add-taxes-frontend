5e2351cf0884aa35e15e8c880090251703b592f3
“nicholas.tate” <nicholas.tate@digital.hmrc.gov.uk>
2018-05-11 14:20:22 +0100

    BEACD-41: started writing test for BEACD-32

--- src/test/resources/features/selfassessment.trusts/SelfAssessment.feature
+++ src/test/resources/features/selfassessment.trusts/SelfAssessment.feature
@@ -15,10 +15,16 @@ Feature: Enrol trust through Self Assessment journey
     When I click continue
     Then I should be redirected to Trusts GovUk page
 
-  Scenario:
+  Scenario: User answers No - decides to add later
     Given I login as an Organisation with NO enrolments
     And I navigate to the trust self assessment page
     When I click No button and continue
     Then I will be redirected to register trust page
     When I click on not-now link
     Then I should be redirected to the /business-account/ page
+@solo
+  Scenario: User answers Yes - add a partner to partnership
+    Given I login as an Organisation with NO enrolments
+    When I navigate to the partnership self assessment page
+    And I click Yes button and continue
+    Then I will be redirected to sa401 pdf
--- src/test/scala/uk/gov/hmrc/integration/cucumber/stepdefs/BaseStepDef.scala
+++ src/test/scala/uk/gov/hmrc/integration/cucumber/stepdefs/BaseStepDef.scala
@@ -1,8 +1,6 @@
 package uk.gov.hmrc.integration.cucumber.stepdefs
 
 import cucumber.api.scala.{EN, ScalaDsl}
-import uk.gov.hmrc.integration.cucumber.utils.methods.Check.{be, driver}
-import uk.gov.hmrc.integration.cucumber.utils.methods.{Check, Nav}
 import uk.gov.hmrc.integration.cucumber.utils.methods.Input._
 
 class BaseStepDef extends ScalaDsl with EN {
@@ -17,7 +15,7 @@ class BaseStepDef extends ScalaDsl with EN {
         case "No" => clickNo
       }
 
-      clickOnContinue()
+      clickContinue()
     }
 
     And("""^I click on (.*) link$""") { (id: String) =>
@@ -28,35 +26,11 @@ class BaseStepDef extends ScalaDsl with EN {
       clickEnrolmentsLink(enrolment)
     }
 
-    Then("""^I will be redirected to emac (.*) enrolments page$""") { (enrolment: String) =>
-      Check.assertEmacUrl(enrolment)
-    }
-
-    Then("""^I will be redirected to register (.*) page$""") { (registerType: String) =>
-      Check.assertRegisterPage(registerType)
-    }
-
     And("""^I click continue$"""){ () =>
       clickContinue()
     }
 
     And("""^I click submit"""){ () =>
       clickSubmit()
-    }
-
-    Then("""^I should be redirected to (.*) GovUk page$""") { (url: String) =>
-      Check.assertGovUk(url)
-    }
-
-    Then("""^I should be redirected to (.*) Portal page$""") { (enrolment: String) =>
-      Check.assertPortalPage(enrolment)
-    }
-
-    Then("""^I should be redirected to the (.*) page$""") { (enrolment: String) =>
-      Check.checkUrlEnd(enrolment)
-    }
-
-    Then("""^I will be redirected to HMCE page$""") { () =>
-      driver.getCurrentUrl should be("https://secure.hmce.gov.uk/ecom/is2/static/is2.html")
     }
 }
