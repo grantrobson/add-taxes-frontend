79a274f70d23558ba83e192dd6027e46370b075f
ryan.niven <ryan.niven@digital.hmrc.gov.uk>
2018-07-12 12:03:10 +0100

    ZAP: added extra logging

--- run_tests_via_zap.sh
+++ run_tests_via_zap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sbt -Dlogback.configurationFile=logback.xml -Dbrowser=zap-chrome-headless 'test-only uk.gov.hmrc.integration.cucumber.utils.Runner'
-sbt 'testOnly uk.gov.hmrc.integration.cucumber.utils.ZapRunner'
+sbt -Dorg.slf4j.simpleLogger.defaultLogLevel=debug 'testOnly uk.gov.hmrc.integration.cucumber.utils.ZapRunner'
